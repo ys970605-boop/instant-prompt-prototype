ca57016c90b24ec69ce3f7001c5845739cc1ebff
용석 <yongseok@MacBook-Air.local>
2026-04-10 16:09:22 +0900

    fix: TanStack Router basepath 설정 (/instant-prompt-prototype)

---
+++ assets/chunk-FMBD7UC4-7PPuz0qq.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./mermaid.core-ChKil5nP.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"568c831b65eb007b73c5f6506d506fd0e43494b1"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="37533312-289a-467b-9347-0a4f6230ddf1",e._sentryDebugIdIdentifier="sentry-dbid-37533312-289a-467b-9347-0a4f6230ddf1")})()}catch{}var t=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{t as g};
