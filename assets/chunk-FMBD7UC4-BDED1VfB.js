4f8b304b334140b2b0ed4b7add1b72aa872fab27
용석 <yongseok@MacBook-Air.local>
2026-04-10 15:56:23 +0900

    feat: 프로토타입 전체 빌드 업데이트 (챗봇/홈페이지/관리자 플로우 추가)
    - 신규 데모 라우트: /chatbot-flow, /homepage-flow, /admin-demo
    - TIPA 예시 1/2 (/start, /start2) 최신 빌드
    - ProfileItem 백엔드 호출을 데모 라우트에서 비활성
    - VITE_IS_PROTOTYPE 스텁 토큰으로 구글 로그인 우회
    - base path: /instant-prompt-prototype/
    - sourcemap 제외

---
+++ assets/chunk-FMBD7UC4-BDED1VfB.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./mermaid.core-mSprZdP_.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"64a8f7dcea624939c3304ac5cb07c0612b281e13"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="37533312-289a-467b-9347-0a4f6230ddf1",e._sentryDebugIdIdentifier="sentry-dbid-37533312-289a-467b-9347-0a4f6230ddf1")})()}catch{}var d=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{d as g};
